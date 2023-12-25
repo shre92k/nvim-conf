7b9d78d41e4cedd55a65b0fda36d37b7b27ccc1c
shre92k <shreyash.work@gmail.com>
2023-12-25 19:07:46 +0530

    adding ripgrep comment

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,6 +10,8 @@ return {
         'nvim-telescope/telescope-fzf-native.nvim',
         -- NOTE: If you are having trouble with this installation,
         --       refer to the README for telescope-fzf-native for more instructions.
+        -- NOTE: Make sure ripgrep is installed_version
+        -- eg. brew install ripgrep
         build = 'make',
         cond = function()
           return vim.fn.executable 'make' == 1
